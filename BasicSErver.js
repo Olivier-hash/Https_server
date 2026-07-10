6861f6beca39656529c46ad75278f1c54157719c
olivier Rukundo <rr.olivier01@gmail.com>
2026-07-10 07:33:51 +0200

    implemented soft delete functionality for users

--- BasicSErver.js
+++ BasicSErver.js
@@ -18,4 +18,4 @@ server.listen(3000, () => {
   console.log("✅ Server is listening on http://localhost:3000");
 });
 
-// dexco start form  integrate custom email validation logic
+// dexc start form  integrate custom email validation logic
